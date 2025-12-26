8ac6b38278e63dc719e6a0e44188ed82883470ec
陈无敌 <yxy_js_code@163.com>
2025-12-26 13:55:27 +0800

    feat: 添加首页英雄区域组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "./components/app/navbar";
+import { HeroSection } from "./components/sections/hero-section";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <HeroSection />
     </div>
   );
 };
