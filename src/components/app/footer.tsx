25248441df18faa28d7f2d3d8dd7d876de07e8ea
陈无敌 <yxy_js_code@163.com>
2025-12-28 11:38:27 +0800

    feat: 完善页脚组件内容与样式

--- src/components/app/footer.tsx
+++ src/components/app/footer.tsx
@@ -1,3 +1,29 @@
+import { footerLinks } from "../../constants";
+
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className="info">
+        <p>
+          更多选购方式：前往附近的 Apple Store
+          零售店，或联系授权经销商。您也可以致电 000800 040 1966。
+        </p>
+        <img src="/logo.svg" alt="Apple logo" />
+      </div>
+
+      <hr />
+
+      <div className="links">
+        <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+
+        <ul>
+          {footerLinks.map(({ label, link }) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 };
